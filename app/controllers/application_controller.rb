558e522349886499868bc001574ede26c434080b
Naftali Kulik <nkulik1994@gmail.com>
2022-06-29 16:26:35 -0400

    commit tree b6aa90de2cab2c75dfd02910bf24c1aea57a74f6

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,28 @@
 class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
 
-  # add routes
+  get "/bakeries" do
+    bakeries = Bakery.all
+
+    bakeries.to_json
+  end
+
+  get '/bakeries/:id' do
+    bakery = Bakery.find params[:id]
+
+    bakery.to_json(include: :baked_goods)
+  end
+
+  def baked_goods_by_price
+    BakedGood.all.order(price: :desc)
+  end
+
+  get '/baked_goods/by_price' do
+    baked_goods_by_price.to_json
+  end
+
+  get '/baked_goods/most_expensive' do
+    baked_goods_by_price.first.to_json
+  end
 
 end
